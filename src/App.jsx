321959c93cd4cd93bae4bfbfdae6b19d664fd2b1
EliteHiro <himhiro5@gmail.com>
2026-04-26 20:50:51 +0530

    Remove Why Chess.VP section

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
             <Header />
             <main>
               <HeroSection />
-              <FeaturesSection />
             </main>
             <Footer />
           </>
